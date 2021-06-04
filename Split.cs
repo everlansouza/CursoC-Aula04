0775a44f7cf9c342221f4f2759bfd33aa14ad8fe
Everlan de Souza <everlansoouza@gmail.com>
2021-06-03 21:53:57 -0300

    Conteudo completo anotado ao decorre da aula 04 do curso de C#

---
+++ Split.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Course4 {
+
+    class Split {
+    
+        static void Main(string[] args) {
+
+            string nome;
+
+            Console.WriteLine("Digite o nome");
+            nome = Console.ReadLine();
+            Console.WriteLine($"Olá {nome}");
+
+            Console.WriteLine("Informe as cores");
+            string[] vet = Console.ReadLine().Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+        }
+    }
+}
